3b19cf1881352202b60b18138b41dcd69b479f57
Ariene Ellefsen <ariene.ellefsen@themedco.com>
2017-04-24 17:02:23 -0400

    adding files js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,24 @@
-//setTimeout(function() {  
-//}, timeout);
-//SetInterval
-//Clear Interval
-//Clear Time out
-
 // 1- Get Dom Elements
 var question = $("#question");
 var inputChoices = $("#choices-input");
 var correctAnswear = $("#answear");
 var timer = $("#timer");
-var seconds =10;
-var score =0;
-var rightAnswear=0;
-var wrongAnswear=0;
-var i=0;
-var num =0;
-var isRight = 1;
- var count= 0;
+var seconds = 10;
+var score = 0;
+var rightAnswear = 0;
+var wrongAnswear = 0;
+var i = 0;
+var num = 0;
+var isRight;
+var count = 0;
+var formChoices = $("#form-choices");
+var message = $("#message");
 
-
-
-
+// Creating question inside of object quiz
 var triviaQuiz = {
     quiz: [{
             questions: "Question 1",
-            choices: ["a", "b", "c"],
+            choices: ["a", "b", "c", "d"],
             correctAnswear: "c",
         }, {
             questions: "Question 2",
@@ -33,136 +27,97 @@ var triviaQuiz = {
         },
         {
             questions: "Question 3",
-            choices: ["1", "2", "3"],
+            choices: ["1", "2", "3", "4", "5", "6", "7"],
             correctAnswear: "1"
         }
     ],
 
-
-    startQuiz: function(i, num, isRight, count) {
-
+    //Create a function to start the quiz
+    startQuiz: function(i) {
 
         //Get the total number of questions
         var holdQuiz = triviaQuiz.quiz;
-        console.log("Questions:" + holdQuiz.length);
+        console.log("Total of questions:" + holdQuiz.length);
 
         //need to get first questions
-        question.html("<p>" +holdQuiz[i].questions  +"</p>")
+        question.html("<p>" + holdQuiz[i].questions + "</p>"); //get first question
+        console.log("Get questions: " + holdQuiz[i].questions);
 
-        //create node for choices
-        //Loop trough choices
-       
+        //Get number of choices
+        var choicesLength = holdQuiz[i].choices.length;
+        console.log("Number of choices:" + choicesLength);
 
-        ;
-        //create choices
-
-
-        for( ;i< 3; i++){
-
-            triviaQuiz.createAnswear(i, holdQuiz, num, isRight, count);       
-
-            }  
-
-         },
-
-         createAnswear: function(i, holdQuiz,num, isRight, count){
+        //Get Choices
+        console.log("Choices:" + holdQuiz[i].choices);
 
         debugger;
 
-       
-            //create radio button
-                var inputRadio = document.createElement("input");
-                inputRadio.setAttribute("type", "radio");
-                inputRadio.setAttribute("name", "choices");
-                inputRadio.setAttribute("value", holdQuiz[count].choices[i]);
-                inputRadio.setAttribute("id","myid");
-          
-                var inputLabel = document.createElement("label");
-                inputLabel.innerHTML = holdQuiz[count].choices[i];
-               
-                var element = document.getElementById("form-choices");
+        //Create a radio button        
+        for (var countChoices = 0; countChoices < choicesLength; countChoices++) {
+            var choicesRadio = holdQuiz[i].choices[countChoices];
+            formChoices.append(choicesRadio + '<input type="radio" name="quizChoices" value="' + choicesRadio + '">');
+        }
 
+        //Display correct answear
+        var quizAnswear = holdQuiz[i].correctAnswear;
+        correctAnswear.text(holdQuiz[i].correctAnswear);
+        console.log("Correct Answear:" + holdQuiz[i].correctAnswear);
+        triviaQuiz.getAnswear(quizAnswear, formChoices);
+    },
 
-                element.appendChild(inputLabel);
-                element.appendChild(inputRadio);
-
-                //display answear
-                correctAnswear.html("Answear: "+holdQuiz[num].correctAnswear);
-                console.log("My questions: " + holdQuiz[num].questions + " Choices: " + holdQuiz[num].choices + " Correct Answear: " + holdQuiz[num].correctAnswear); // get questions
-                var myAnswear = holdQuiz[num].correctAnswear;
-                console.log("correct answear2 : " + myAnswear);
-                triviaQuiz.getAnswear(myAnswear, num, element);
-
-                //check if there is timeout
-
-         },
-
-            //Check if answear is correct
-
-            getAnswear: function(myAnswear, num, element){
-            $('#form-choices input[type=radio]').change(function() {       
-               
-                var userAnswear = this.value;
-
-                console.log("User Answear: " + this.value);
-                console.log("Game Answear: ");
-
-
-                if(myAnswear === userAnswear){
-                    console.log('Correct Answear');
-                    isRight === true;
-                    rightAnswear += rightAnswear +1;
-                    console.log("Right Answear score" + rightAnswear);
-                    triviaQuiz.nextQuestion(i, num, isRight, count);
-                }
-
-                else{
-                    rightAnswear = false;
-                    wrongAnswear +1;
-                    console.log("Wrong Answear score" + wrongAnswear);
-
-                    console.log("Nope");
-                }
-
-                console.log(userAnswear);
-            });
-            },
-
-            nextQuestion: function(i, num,isRight, count){
-        
-        count=0;
-                i = i+1;
-                num = num +1;
-
-
-
-                triviaQuiz.startQuiz(i, num, isRight, count);
-                //move to next question
-                console.log("next question: " +i);
-
-            },
-
-            setTime: function() {
-            seconds --;
-            timer.html(seconds + " seconds");
-
-            if (seconds === 0){
-            
-                clearTimeout(myVar);
-
-                console.log("game over");
-
-
+    //Get Anwear form user's input
+    getAnswear: function(quizAnswear, formChoices) {
+        $('#form-choices input[type=radio]').change(function() {
+            var userAnswear = this.value;
+            console.log("User's answear: " + userAnswear);
+            //Validate if answears is correct
+            if (userAnswear === quizAnswear) {
+                isRight = true;
+                console.log("Correct answear");
+                //call function to update score 
+                triviaQuiz.updateScore(isRight, i, formChoices);
+            } else {
+                isRight = false;
+                console.log("Nope");
+                triviaQuiz.updateScore(isRight, i);
             }
-            else{
-                console.log("Playing!!");
-            }
+        });
+    },
 
-            },
+    updateScore: function(isRight, i, formChoices) {
+        if (isRight === true) {
+            rightAnswear = rightAnswear + 1;
+            message.text("Great job!");
+            console.log("Score Right Answear:" + rightAnswear);
+            //move to next question
+            setTimeout(function myFunction() {
+                alert('Was called after 2 seconds');
+            }, 2000);
+            setTimeout(triviaQuiz.nextQuestion(i, formChoices), 20000)
 
-        };
+        } else if (isRight === false) {
+            wrongAnswear = wrongAnswear + 1;
+            message.text("Wrong Answear!");
+            console.log("Score Wrong Answear:" + wrongAnswear);
+            triviaQuiz.nextQuestion(i, formChoices);
+        }
+    },
 
-triviaQuiz.startQuiz(i, num, isRight, count);
+    nextQuestion: function() {
+        i = +i + 1;
+        console.log("I: " + i);
+        formChoices.empty();
+        triviaQuiz.startQuiz(i);
+    },
+};
+
+triviaQuiz.startQuiz(i);
 var time = setTimeout(triviaQuiz.setTime, 5000);
 //triviaQuiz.setTime();
 var myVar = setInterval(triviaQuiz.setTime, 1000);
+
+//Todo list
+// Validate if time has exceded
+// Slow next question 
+// Verify end of question
+// Show ansears and score at the end of the game
