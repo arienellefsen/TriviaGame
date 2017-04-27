f03935c1cbd4d855de729ab2d2149158c19021e2
Ariene Ellefsen <ariene.ellefsen@gmail.com>
2017-04-27 14:44:48 -0400

    adding files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,18 +18,16 @@ var start = $("#start");
 var instructions = $("#instructions");
 var card = $("#card");
 
-
-//Creating start
+//1- Start game after click on button
 $("#start").on( "click", function() {
     instructions.fadeOut( "slow" ,function() {
     card.css("display","block").fadeIn("slow");
-  });
     triviaQuiz.init(i);
-            var intervalId = setInterval(triviaQuiz.gameRun, 1000);
-
+  });
 });
 
-// Creating question inside of object quiz
+
+//2- Creating question inside of object quiz
 var triviaQuiz = {
     quiz: [{
             questions: "When Central Park was first opened to the public?",
@@ -39,175 +37,156 @@ var triviaQuiz = {
             questions: "What is the measurement of the Statue of Liberty?",
             choices: ["205 feet 1/2 inches from the ground to the tip of the flame", "305 feet 1 inch from the ground to the tip of the flame", "310 feet 2 inches from the ground to the tip of the flame"],
             correctAnswear: "305 feet 1 inch from the ground to the tip of the flame",
+        },
+        {
+            questions: "What NYC museum started in Gracie Mansion and later moved to 5th Ave. between 103rd St. and 104th St.?",
+            choices: ["New York Hall of Science", "Metropolitan Museum of Art", "New York Transit Museum", "Museum of The City of New York"],
+            correctAnswear: "Museum of The City of New York"
+        },
+
+       {
+            questions: "The New York Aquarium is located in which New York City borough?",
+            choices: ["Brooklyn", "Queens", "Bronx", "Manhattan"],
+            correctAnswear: "Brooklyn"
         }
-       
     ],
 
-    //Create a function to start the quiz
+    //3 - Create a function to start the quiz
     startQuiz: function(i) {
+    var myVar = setInterval(function(){ triviaQuiz.gameRun() }, 1000)
 
         //Get the total number of questions
         var holdQuiz = triviaQuiz.quiz;
-        console.log("Total of questions:" + holdQuiz.length);
-
-
         //if is equal undefinied it means is the last question
-         //Check if is the last question
-
         if(holdQuiz.length === i){
-            console.log("end questions");
             triviaQuiz.gameOver();
 
          } else if (holdQuiz.length !== i){
 
-            //getTime
-
         question.html("<p>" + holdQuiz[i].questions + "</p>"); //get first question
-        console.log("Get questions: " + holdQuiz[i].questions);
-
         //Get number of choices
         var choicesLength = holdQuiz[i].choices.length;
-        console.log("Number of choices:" + choicesLength);
 
         //Get Choices
-        console.log("Choices:" + holdQuiz[i].choices);
         for (var countChoices = 0; countChoices < choicesLength; countChoices++) {
             var choicesRadio = holdQuiz[i].choices[countChoices];
             formChoices.append('<input type="radio" name="quizChoices" id="quizChoices" value="' + choicesRadio + '">' + choicesRadio+ "<br>" );
         }
         //Display correct answear
         var quizAnswear = holdQuiz[i].correctAnswear;
-        console.log("Correct Answear:" + holdQuiz[i].correctAnswear);
-        triviaQuiz.getAnswear(quizAnswear, formChoices, quizAnswear, seconds);
-
-
+        triviaQuiz.getAnswear(quizAnswear, formChoices, quizAnswear, myVar);
         }        
     },
 
-    //Get Anwear form user's input
-    getAnswear: function(quizAnswear, formChoices) {
-
-
+    //4 - Get Anwear form user's input
+    getAnswear: function(quizAnswear, formChoices,myVar) {
         $('#form-choices input[type=radio]').change(function() {
             var userAnswear = this.value;
-            console.log("User's answear: " + userAnswear);
-
-            //Validate if answears is correct
-            
-
+            //Check if answears is correct       
             if (userAnswear === quizAnswear) {
                 isRight = true;
-
-                console.log("Correct answear");
-                //call function to update score 
+                //Call function to update score 
                 triviaQuiz.updateScore(isRight, i, formChoices, quizAnswear);
             } 
 
-    
+            else if(userAnswear === ""){
+
+                isRight = null;
+                //call function to update score 
+                triviaQuiz.updateScore(isRight, i, formChoices, quizAnswear);
+            }
             else {
                 isRight = false;
-                console.log("Nope");
                 triviaQuiz.updateScore(isRight, i,formChoices, quizAnswear);
             }
-            
         });
     },
-
+    //5 - Update Score
     updateScore: function(isRight, i, formChoices, quizAnswear) {
-       
+        var holdQuiz = triviaQuiz.quiz;
         if (isRight === true) {
-
             triviaQuiz.getScore(isRight);
-
             message.html("Great job!" + " <i class='fa fa-thumbs-up icon' aria-hidden='true'></i>");
-
             //create a function to stop time when the answear is correct and time less than 20 seconds
             correctAnswear.text("The correct answear is: " +quizAnswear);
-
-            //move to next question
-            
+            //move to next question           
             setTimeout(function myFunction() {
                 triviaQuiz.nextQuestion(i, formChoices);
             }, 5000);
 
-
         } else if (isRight === false) {
-
             triviaQuiz.getScore(isRight);
-
             message.html("Hummmm! That is not the correct answear! " + " <i class='fa fa-thumbs-down icon' aria-hidden='true'></i>");
             correctAnswear.html("The correct answear is: " +quizAnswear);
-
             setTimeout(function delayNext() {
                 triviaQuiz.nextQuestion(i, formChoices);
             }, 5000);
         }
 
-
-    
+        else if(isRight === null){
+         triviaQuiz.getScore(isRight);
+         message.html("Hummmm! You did not choose an answear! " + " <i class='fa fa-thumbs-down icon' aria-hidden='true'></i>");
+            correctAnswear.html("The correct answear is: " + holdQuiz[i].correctAnswear);
+                setTimeout(function delayNext() {
+                triviaQuiz.nextQuestion(i, formChoices);
+            }, 5000);
+        }
     },
-
+// 6 - Go to next question
     nextQuestion: function() {
         seconds = 10;
         message.text("");
         i = +i + 1;
-        console.log("I: " + i);
         formChoices.empty();
         correctAnswear.text("");
         triviaQuiz.startQuiz(i);
     },
 
+// 7 - Update Score
+
     getScore: function(isRight){
-
-        console.log("is right" + isRight);
-
         if (isRight === true) {
             rightAnswear = rightAnswear + 1;
-
         }else if (isRight === false)  {
             wrongAnswear = wrongAnswear + 1;
+        }else  if (isRight === null)  {
+            noAnwear = noAnwear + 1;
         }
     },
+// 8 - Reset game
 
     gameOver: function(){
-        console.log("Game Over");
         timer.text(" ");
         message.text("Game over");
         timer.css("display", "none");
-
         question.css("display", "none");
         formChoices.empty();
         correctAnswear.text("");
         triviaQuiz.getScore(rightAnswear,wrongAnswear );
         $("#score").css("display", "block").html("Correct Answears: " + rightAnswear + "<br> Incorrect Answears: " 
         + wrongAnswear + "<br>" + "No Answear:" 
-        + noAnwear);
-
-
+        + noAnwear)
     },
 
+// 9 - Start game
     init: function(quizTime){
         triviaQuiz.startQuiz(i);
-
     },
-
+// 10 - update game time
     gameRun: function(){
+        //Update seconds
         seconds = seconds -1;
 
         if (seconds >= 0){
-
-            timer.text("Remaining: " + seconds);
+        timer.text("Remaining: " + seconds);
         } 
-        
 
-    },
-
-    stoptime: function(quizTime){
-        
+        else{
+            timer.text("Time Out!");
+            isRight = null;
+        }
     },
 };
-
-//get times
 
 
 
